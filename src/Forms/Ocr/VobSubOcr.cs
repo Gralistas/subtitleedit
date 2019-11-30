de1283e5c2428f7b49d818fbf48d8d003823dd80
Waldi Ravens <wr86420@gmail.com>
2019-11-30 05:03:12 +0100

    Collect Tesseract stderr messages

--- src/Forms/Ocr/VobSubOcr.cs
+++ src/Forms/Ocr/VobSubOcr.cs
@@ -5547,7 +5547,6 @@ namespace Nikse.SubtitleEdit.Forms.Ocr
         {
             if (_tesseractRunner == null)
             {
-                //_tesseractThreadRunner = new TesseractThreadRunner(OcrDone);
                 _tesseractRunner = new TesseractRunner();
             }
 
